a3c82c7e87f7b9b8e11bec2650b1b40cf950df8b
Curtis Carter <lokiphasma@gmail.com>
2019-10-19 10:38:53 -0500

    Added 64-bit installer

--- transgui/update.ps1
+++ transgui/update.ps1
@@ -6,7 +6,9 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
         "$($Latest.PackageName).nuspec" = @{
             "(\<releaseNotes\>\[Release Notes\]\().*?(\)\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
@@ -21,11 +23,14 @@ function global:au_GetLatest {
     $re    = '\.exe$'
     $url   = $download_page.links | ? href -match $re | select -Expand href -First 1 | % { 'https://github.com' + $_ }
     $version = (Split-Path $url -Parent).Split("v")[1]
+    $url64   = "https://github.com/transmission-remote-gui/transgui/releases/download/v$version/transgui-$version-setup_64bit.exe"
+
     @{
         Version      = $version
         URL32        = $url
+        URL64        = $url64
         ReleaseNotes = "https://github.com/transmission-remote-gui/transgui/releases/tag/v${version}"
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor all
